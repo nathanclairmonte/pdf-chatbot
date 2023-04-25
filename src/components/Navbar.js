7a0c1bd64355ac7789a0c38997bd45f35229646a
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-25 19:32:24 -0400

    updated Navbar + Home to work with auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,27 @@
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
-const Navbar = ({ apiKey, setApiKey }) => {
+const Navbar = ({ apiKey, setApiKey, session }) => {
     return (
         <div className="flex flex-col gap-1 border-b-2 border-b-[#eb9722] bg-zinc-900">
             <div className="flex flex-row items-center justify-between px-3 py-4">
                 <Link href="/" className="ml-4 text-xl text-[#eb9722] hover:opacity-80">
                     PDF ChatBot
                 </Link>
-                <input
-                    autoFocus={false}
-                    rows={1}
-                    type="password"
-                    id="apiKey"
-                    name="apiKey"
-                    placeholder="Enter your OpenAI API key here"
-                    value={apiKey}
-                    onChange={(e) => setApiKey(e.target.value)}
-                    className="ml-4 hidden h-11 w-2/4 max-w-xl resize-none rounded border-zinc-800 bg-zinc-200 p-4 text-lg text-zinc-900 outline-none md:flex"
-                />
-                <div className="mr-4 flex flex-row justify-end">
+                {session ? (
+                    <input
+                        autoFocus={false}
+                        rows={1}
+                        type="password"
+                        id="apiKey"
+                        name="apiKey"
+                        placeholder="Enter your OpenAI API key here"
+                        value={apiKey}
+                        onChange={(e) => setApiKey(e.target.value)}
+                        className="ml-4 hidden h-11 w-2/4 max-w-xl resize-none rounded border-zinc-800 bg-zinc-200 p-4 text-lg text-zinc-900 outline-none md:flex"
+                    />
+                ) : null}
+                <div className="mr-4 flex flex-row items-center justify-end gap-5">
                     <Link
                         href="https://github.com/nathanclairmonte/pdf-chatbot"
                         className="text-xl text-[#eb9722] hover:opacity-80"
@@ -27,21 +30,31 @@ const Navbar = ({ apiKey, setApiKey }) => {
                     >
                         Repo
                     </Link>
+                    {session ? (
+                        <button
+                            className="text-md rounded-md bg-[#eb9722] px-3 py-2 text-zinc-50 hover:opacity-80"
+                            onClick={() => signOut()}
+                        >
+                            Sign Out
+                        </button>
+                    ) : null}
                 </div>
             </div>
-            <div className="flex flex-row justify-center px-4 pb-4 md:hidden">
-                <input
-                    autoFocus={false}
-                    rows={1}
-                    type="password"
-                    id="apiKey"
-                    name="apiKey"
-                    placeholder="Enter your OpenAI API key here"
-                    value={apiKey}
-                    onChange={(e) => setApiKey(e.target.value)}
-                    className="h-11 w-2/4 max-w-xl flex-grow resize-none rounded border-zinc-800 bg-zinc-200 p-4 text-lg text-zinc-900 outline-none"
-                />
-            </div>
+            {session ? (
+                <div className="flex flex-row justify-center px-4 pb-4 md:hidden">
+                    <input
+                        autoFocus={false}
+                        rows={1}
+                        type="password"
+                        id="apiKey"
+                        name="apiKey"
+                        placeholder="Enter your OpenAI API key here"
+                        value={apiKey}
+                        onChange={(e) => setApiKey(e.target.value)}
+                        className="h-11 w-2/4 max-w-xl flex-grow resize-none rounded border-zinc-800 bg-zinc-200 p-4 text-lg text-zinc-900 outline-none"
+                    />
+                </div>
+            ) : null}
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { useEffect, useRef, useState } from "react";
 import { Navbar, MessagesList, QueryInput, PDFInput } from "@/components/list";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,27 +24,34 @@ export default function Home() {
     const messageListRef = useRef(null);
     const queryInputRef = useRef(null);
 
+    // auth session
+    // const { data: session } = useSession()
+    // const session = true;
+    const session = false;
+
     // auto scroll chat to bottom whenever messages state changes
     useEffect(() => {
-        // put ref into variable (for readability)
-        const messageList = messageListRef.current;
+        if (session) {
+            // put ref into variable (for readability)
+            const messageList = messageListRef.current;
 
-        // save current window scroll position
-        const prevWinScrollY = window.scrollY;
+            // save current window scroll position
+            const prevWinScrollY = window.scrollY;
 
-        // scroll chat to bottom
-        messageList.scrollTop = messageList.scrollHeight;
+            // scroll chat to bottom
+            messageList.scrollTop = messageList.scrollHeight;
 
-        // refocus on the query input
-        queryInputRef.current.focus();
+            // refocus on the query input
+            queryInputRef.current.focus();
 
-        // restore window scroll position
-        window.scrollTo(0, prevWinScrollY);
+            // restore window scroll position
+            window.scrollTo(0, prevWinScrollY);
+        }
     }, [messages]);
 
     // keep history up to date (and doesn't exceed 2 messages in length)
     useEffect(() => {
-        if (messages.length > 2) {
+        if (messages.length > 2 && session) {
             setHistory([messages[messages.length - 2].text, messages[messages.length - 1].text]);
         }
     }, [messages]);
@@ -55,23 +64,35 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Navbar apiKey={apiKey} setApiKey={setApiKey} />
+            <Navbar apiKey={apiKey} setApiKey={setApiKey} session={session} />
             <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-                <PDFInput setDocs={setDocs} setMessages={setMessages} />
-                <MessagesList
-                    messages={messages}
-                    messageListRef={messageListRef}
-                    loading={loading}
-                />
-                <QueryInput
-                    loading={loading}
-                    queryInputRef={queryInputRef}
-                    history={history}
-                    docs={docs}
-                    apiKey={apiKey}
-                    setLoading={setLoading}
-                    setMessages={setMessages}
-                />
+                {session ? (
+                    <>
+                        <PDFInput setDocs={setDocs} setMessages={setMessages} />
+                        <MessagesList
+                            messages={messages}
+                            messageListRef={messageListRef}
+                            loading={loading}
+                        />
+                        <QueryInput
+                            loading={loading}
+                            queryInputRef={queryInputRef}
+                            history={history}
+                            docs={docs}
+                            apiKey={apiKey}
+                            setLoading={setLoading}
+                            setMessages={setMessages}
+                        />
+                    </>
+                ) : (
+                    <div className="flex flex-row gap-2 text-2xl text-zinc-50">
+                        Please{" "}
+                        <Link href="/auth/signin" className="text-[#eb9722] hover:opacity-80">
+                            Sign in
+                        </Link>
+                        to use the PDF ChatBot!
+                    </div>
+                )}
             </main>
         </>
     );
