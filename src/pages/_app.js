26677a8c17bd192e771ee4516a06fec64fc8a86f
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-25 19:32:04 -0400

    added next-auth config (preliminary)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />;
+        </SessionProvider>
+    );
 }
