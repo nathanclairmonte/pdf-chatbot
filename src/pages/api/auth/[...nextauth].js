2306a2c8bd853c17f5d6e6404a0cc583770516b4
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-27 18:19:56 -0400

    added CredentialsProvider to next-auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+// import connectMongo from "@/lib/connect";
+// import Users from "@/lib/authSchema";
+// import { compare } from "bcryptjs";
+import { signinUser } from "@/lib/authUtils";
 
 export const authOptions = {
     // Configure authentication providers
@@ -8,7 +13,24 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
+        CredentialsProvider({
+            name: "Credentials",
+            // credentials: {},
+            async authorize(credentials, _) {
+                try {
+                    const { email, password } = credentials;
+                    const { user, error } = await signinUser(email, password);
+                    if (error) throw new Error(error);
+                    return user;
+                } catch (error) {
+                    return null;
+                }
+            },
+        }),
     ],
+    // pages: {
+    //     signIn: "/auth/signin"
+    // }
 };
 
 export default NextAuth(authOptions);
