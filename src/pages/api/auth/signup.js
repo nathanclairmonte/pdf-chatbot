2e6580f8f0873401230b4d513214fc85527751b4
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-27 22:40:35 -0400

    updated error reporting for signup API route

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -3,31 +3,31 @@ import Users from "@/lib/authSchema";
 import { hash } from "bcryptjs";
 
 export default async function handler(req, res) {
+    // output variable
+    const output = { user: null, error: null, ok: false };
+
     // connect to our MongoDB instance
-    connectMongo().catch((error) => res.json({ error: `Connection failed :( \n ${error})` }));
+    connectMongo().catch((error) =>
+        res.json({ ...output, error: `Connection failed :( \n ${error})` })
+    );
 
     // accept only POST requests
     if (req.method === "POST") {
         // ensure we have user input
         if (!req.body) {
-            return res.status(404).json({ error: "No form data given." });
+            return res.status(404).json({ ...output, error: "No form data given." });
         }
 
         // get data from user input
-        const { username, email, password } = req.body;
+        const { name, email, password, authorized } = req.body;
 
         // check duplicate users by email
         const existingUsersEmail = await Users.findOne({ email });
         if (existingUsersEmail) {
+            console.log({ ...output, error: "User with that email address already exists." });
             return res
                 .status(422)
-                .json({ message: "User with that email address already exists." });
-        }
-
-        // check duplicate users by username
-        const existingUsersUsername = await Users.findOne({ username });
-        if (existingUsersUsername) {
-            return res.status(422).json({ message: "User with that username already exists." });
+                .json({ ...output, error: "User with that email address already exists." });
         }
 
         // hash password
@@ -38,16 +38,17 @@ export default async function handler(req, res) {
 
         // Save user info to database
         try {
-            const user = await Users.create({ username, email, password: hashedPassword });
-            return res.status(201).json({ status: true, user });
+            const user = await Users.create({ name, email, authorized, password: hashedPassword });
+            return res.status(201).json({ ...output, ok: true, user });
         } catch (error) {
-            return res.status(404).json({ error: `Something went wrong :( \n ${error}` });
+            return res
+                .status(404)
+                .json({ ...output, error: `Something went wrong :( \n ${error}` });
         }
     } else {
-        return res
-            .status(500)
-            .json({
-                message: `HTTP method ${req.method} not valid. Only POST requests are allowed.`,
-            });
+        return res.status(500).json({
+            ...output,
+            error: `HTTP method ${req.method} not valid. Only POST requests are allowed.`,
+        });
     }
 }
