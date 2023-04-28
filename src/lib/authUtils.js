6175e1c78c952873a3d4eabe6da8b0fe5ec4464e
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-27 22:39:23 -0400

    added nextAuthSigninCallback function

--- src/lib/authUtils.js
+++ src/lib/authUtils.js
@@ -33,3 +33,66 @@ export const signinUser = async (userInputEmail, userInputPassword) => {
     // if all checks pass, return user
     return { ...output, user };
 };
+
+export const nextAuthSigninCallback = async ({ user, account, profile }) => {
+    // console.log("starting");
+    // console.log("user:", user);
+    // console.log("account:", account);
+    // console.log("profile:", profile);
+    // variables for the pages we will redirect to. just for readability.
+    const homePage = `${process.env.NEXTAUTH_URL}/`;
+    const unauthorizedPage = `${process.env.NEXTAUTH_URL}/unauthorized`;
+
+    // if we didn't get an account.type, something went wrong. redirect to home.
+    if (!account.provider || !user) {
+        // console.log("didn't get account.provider");
+        return homePage;
+    }
+
+    // connect to MongoDB instance
+    connectMongo().catch((error) => {
+        // console.log(`MongoDB connection failed \n ${error}`);
+        return homePage;
+    });
+
+    // check if user exists in db
+    // console.log("checking db...");
+    try {
+        const dbUser = await Users.findOne({ email: user.email });
+
+        if (dbUser) {
+            // if user exists in db, return true if they are authorized to sign in, otherwise redirect to unauthorizedPage
+            // (regardless of whether the provider is credentials or oauth)
+            // console.log("found user");
+            return dbUser.authorized && dbUser.authorized === true ? true : unauthorizedPage;
+        } else {
+            // if user doesn't exist in db, check provider type.
+            // (NB: might have to update if we add another oauth provider. {profile.name} might be called something else.)
+            if (account.provider === "credentials") {
+                // if credentials type, there's a problem somewhere. user should already exist in db.
+                // the user has probably mistyped their email address.
+                // return true to let sign in process continue normally, will fail later on (and be caught by frontend)
+                return true;
+            } else {
+                // if oauth type, this is the first time user is signing in. (otherwise they'd exist already in db)
+                try {
+                    // add user info to db
+                    await Users.create({
+                        name: `${profile.name}`,
+                        email: user.email,
+                        password: "", // empty password for oauth users
+                        authorized: false, // new users unauthorized by default
+                    });
+
+                    // redirect to unauthorized page (since new user, by default won't be authorized)
+                    return unauthorizedPage;
+                } catch (error) {
+                    // error creating user in DB. halt signin process and return home
+                    return homePage;
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
