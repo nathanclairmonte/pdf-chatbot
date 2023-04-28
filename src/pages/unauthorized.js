53b530f6b30e14d3a49a7c19e814fa076899a9f6
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-28 14:48:14 -0400

    created unauthorized page

--- src/pages/unauthorized.js
+++ src/pages/unauthorized.js
@@ -1,6 +1,7 @@
 import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Unauthorized = () => {
     // get auth session
@@ -12,7 +13,29 @@ const Unauthorized = () => {
             signOut();
         }
     }, [session]);
-    return <div>BANNED!</div>;
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center gap-6 border border-white p-24 text-zinc-50">
+            <h1 className="text-center text-6xl">Sign in/sign up successful!</h1>
+            <h2 className="text-center text-2xl">
+                Now you just need to be approved, please{" "}
+                <Link
+                    href="mailto:nathanclairmonte@outlook.com"
+                    className="text-[#eb9722] hover:opacity-80"
+                >
+                    contact Nathan
+                </Link>{" "}
+                to get your account approved.
+            </h2>
+            <h2 className="text-center text-xl">
+                In the meantime, feel free to try{" "}
+                <Link href="/" className="text-[#eb9722] hover:opacity-80">
+                    chatting with our sample PDFs
+                </Link>
+                !
+            </h2>
+        </main>
+    );
 };
 
 export default Unauthorized;
