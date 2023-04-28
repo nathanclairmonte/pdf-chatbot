ecb4fb909fcaac1aca35dc69d56514014a8e554f
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-27 23:28:08 -0400

    `/unauthorized` page signs user out on load

--- src/pages/unauthorized.js
+++ src/pages/unauthorized.js
@@ -1,4 +1,17 @@
+import { signOut } from "next-auth/react";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+
 const Unauthorized = () => {
+    // get auth session
+    const { data: session } = useSession();
+
+    // sign user out if they are signed in.
+    useEffect(() => {
+        if (session) {
+            signOut();
+        }
+    }, [session]);
     return <div>BANNED!</div>;
 };
 
